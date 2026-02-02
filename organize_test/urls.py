4f7b51796d98ea07ca40090666f95c72ba4240d9
vishalipar <vishalipar04@gmail.com>
2026-02-03 00:14:36 +0530

    Designed Manage test page to add questions to the test created.

--- organize_test/urls.py
+++ organize_test/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.organize_test, name='organize_test'),
     path('add_question/', views.add_question, name='add_question'),
+    path('manage_test/', views.manage_test, name='manage_test'),
 ]
--- organize_test/views.py
+++ organize_test/views.py
@@ -7,3 +7,6 @@ def organize_test(request):
     
 def add_question(request):
     return render(request, 'add_question.html')
+    
+def manage_test(request):
+    return render(request, 'manage_test.html')
