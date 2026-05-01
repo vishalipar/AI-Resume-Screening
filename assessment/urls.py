743d835a407bb8e1135a887df227c4c44ed8a588
vishalipar <vishalipar04@gmail.com>
2026-05-02 01:17:42 +0530

    Designed test Instruction page and actual test page with the timer

--- assessment/urls.py
+++ assessment/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('<uuid:token>/', views.assessment_test, name='assessment_test'),
+    path('<uuid:token>/start/', views.start_test, name='start_test'),
+    path('<uuid:token>/take/', views.take_test, name='take_test'),
 ]
--- assessment/views.py
+++ assessment/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404,HttpResponse
 from .models import TestAttempt
+from organize_test.models import QuestionModel
 # Create your views here.
 
 def assessment_test(request, token):
@@ -12,5 +13,37 @@ def assessment_test(request, token):
     request.session['is_candidate'] = True
 
     return render(request, 'assessment_test.html', {
-        'test': attempt.test
+        'test': attempt.test,
+        'attempt': attempt
     })
+    
+def start_test(request, token):
+    attempt = TestAttempt.objects.get(token=token)
+    # mark as started
+    attempt.status = "started"
+    attempt.save()
+    request.session['attempt_id'] = attempt.id
+    return redirect('take_test', token=token)
+    
+def take_test(request, token):
+    attempt = get_object_or_404(TestAttempt, token=token)
+    # optional: prevent reattempt
+    # if attempt.submitted:
+    #     return HttpResponse("You already submitted this test")
+    
+    questions = QuestionModel.objects.filter(
+        test=attempt.test,
+        is_selected=True
+    )
+
+    # mark started
+    attempt.started = True
+    attempt.save()
+    total_marks = sum(q.marks for q in questions)
+    return render(request, "take_test.html", {
+        "attempt": attempt,
+        "test": attempt.test,
+        "questions": questions,
+        "total_marks": total_marks
+    })
+    
